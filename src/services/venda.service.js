0da55bba6cda443f721f45db5e823988a8b41a5d
NovettiGustavo <gustavonovettidev@gmail.com>
2025-08-25 16:51:37 -0300

    Criado o service com validações para a tabela de vendas

---
+++ src/services/venda.service.js
@@ -0,0 +1,27 @@
+const vendaRepository = require("../repositories/venda.repository");
+
+class vendaService{
+    async getVendaById(id){
+        if(!id){
+            throw new Error("Missign id");
+        }
+
+        const venda = await vendaRepository.findVendaById(id);
+
+        if(!venda){
+            throw new Error("Venda not founded!");
+        }
+
+        if(!venda.cliente){
+            throw new Error("Venda does not have an associated cliente")
+        }
+
+        return venda;
+    }
+
+    async getAllVendas(){
+        const vendas = await vendaRepository.prisma.findAll();
+
+        return vendas;
+    }
+}
