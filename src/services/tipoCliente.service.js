3ae0d6743d53e6b7a1bba3d43f950ddbf0a70e93
NovettiGustavo <gustavonovettidev@gmail.com>
2025-08-27 15:11:48 -0300

    Create tipocliente implementado na camada service

--- src/services/tipoCliente.service.js
+++ src/services/tipoCliente.service.js
@@ -24,6 +24,26 @@ class TipoClienteService {
 
         return tipoClientes;
     }
+
+    async createTipoCliente(data) {
+        const { s_dsctipocliente_tipocliente } = data;
+
+        try {
+            if (!s_dsctipocliente_tipocliente) {
+                throw new Error("tipoCliente description is required")
+            }
+
+            const newTipoCliente = await tipoClienteRepository.createTipoCliente({
+                ...data
+            })
+
+            return newTipoCliente
+        } catch (error) {
+            console.error("Error in TipoCliente Service:", error.message);
+            throw new Error("Failed to create TipoCliente");
+        }
+    }
+
 }
 
 module.exports = new TipoClienteService();
