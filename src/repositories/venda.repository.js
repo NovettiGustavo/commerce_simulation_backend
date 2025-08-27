417e5836ab603a0207dbe49aac368dbd5f55afb2
NovettiGustavo <gustavonovettidev@gmail.com>
2025-08-26 23:03:18 -0300

    Criado createVenda na camada repository

--- src/repositories/venda.repository.js
+++ src/repositories/venda.repository.js
@@ -36,6 +36,23 @@ class VendaRepository {
         return venda;
     }
 
+    async createVenda(data){
+        try{
+            const newVenda = await this.prisma.venda.create({
+                data:{
+                    d_data_venda: data.d_data_venda ? new Date(data.d_data_venda) : null,
+                    f_valor_venda: data.f_valor_venda,
+                    i_cliente_cliente: data.i_cliente_cliente
+                }
+            })
+
+            return newVenda;
+        }catch(error){
+            console.error("Error create venda", error.message);
+            throw new Error("Error on create venda on database")
+        }
+    }
+
 }
 
 module.exports = new VendaRepository(prisma)
