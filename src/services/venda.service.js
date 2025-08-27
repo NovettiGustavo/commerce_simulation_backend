8fef22d4f7c2ac80174a0e79bb239b707f458004
NovettiGustavo <gustavonovettidev@gmail.com>
2025-08-26 23:19:31 -0300

    Criado o createVenda na camada service

--- src/services/venda.service.js
+++ src/services/venda.service.js
@@ -1,28 +1,66 @@
+const clienteRepository = require("../repositories/cliente.repository");
 const vendaRepository = require("../repositories/venda.repository");
 
-class vendaService{
-    async getVendaById(id){
-        if(!id){
+class vendaService {
+    async getVendaById(id) {
+        if (!id) {
             throw new Error("Missign id");
         }
 
         const venda = await vendaRepository.findVendaById(id);
 
-        if(!venda){
+        if (!venda) {
             throw new Error("Venda not founded!");
         }
 
-        if(!venda.cliente){
+        if (!venda.cliente) {
             throw new Error("Venda does not have an associated cliente")
         }
 
         return venda;
     }
 
-    async getAllVendas(){
+    async getAllVendas() {
         const vendas = await vendaRepository.findAll();
 
         return vendas;
+    }
+
+    async createVenda(data) {
+        const { d_data_venda, f_valor_venda, i_cliente_cliente } = data;
+
+        try {
+            if (!d_data_venda || !f_valor_venda) {
+                throw new Error("Missing required fields")
+            }
+
+            if (!i_cliente_cliente) {
+                throw new Error("A sale can only be created associated with an customer")
+            }
+
+            const valorVenda = Number(f_valor_venda);
+            if(isNaN(valorVenda)){
+                throw new Error("Sales value must be a valid number");
+            }
+
+            if(isNaN(new Date(d_data_venda).getDate())){
+                throw new Error("Sale date invalid!");
+            }
+
+            const cliente = await clienteRepository.findClienteById(i_cliente_cliente);
+            if(!cliente){
+                throw new Error("Cliente not found!")
+            }
+
+            const newVenda = await vendaRepository.createVenda({
+                ...data,
+               f_valor_venda: valorVenda
+            })
+
+            return newVenda;
+        } catch (error){
+            throw new Error(error.message)
+        }
     }
 }
 
