6f75e31423f59191b01a79f0f2d79e3046ce9dd7
NovettiGustavo <gustavonovettidev@gmail.com>
2025-08-26 23:29:32 -0300

    createVenda criado na camada controller e passado para venda.route

--- src/controllers/venda.controller.js
+++ src/controllers/venda.controller.js
@@ -1,33 +1,55 @@
 const vendaService = require("../services/venda.service")
 
-class VendaController{
-    async getVendaById(req,res){
-        try{
-            const {id} = req.params;
+class VendaController {
+    async getVendaById(req, res) {
+        try {
+            const { id } = req.params;
             const venda = await vendaService.getVendaById(Number(id));
 
-            if(!venda){
-                return res.status(404).json({message:"Venda not founded"});
+            if (!venda) {
+                return res.status(404).json({ message: "Venda not founded" });
             }
 
             return res.status(200).json(venda)
-        }catch(error){
-            return res.status(500).json({error: error.message})
+        } catch (error) {
+            return res.status(500).json({ error: error.message })
         }
     }
 
-    async getAllVendas(req,res){
-        try{
+    async getAllVendas(req, res) {
+        try {
             const vendas = await vendaService.getAllVendas();
-            
-            if(vendas.length === 0){
-                return res.status(404).json({message: "Vendas not founded"})
+
+            if (vendas.length === 0) {
+                return res.status(404).json({ message: "Vendas not founded" })
             }
 
             return res.status(200).json(vendas)
 
+        } catch (error) {
+            return res.status(500).json({ error: error.message })
+        }
+    }
+
+    async createVenda(req, res) {
+        
+        try {
+            const { d_data_venda, f_valor_venda, i_cliente_cliente } = req.body;
+
+            if(!d_data_venda || !f_valor_venda || !i_cliente_cliente){
+                return res.status(400).json({message: "Missing required fields on create Venda!"});
+            }
+
+            const newVenda = await vendaService.createVenda({
+                d_data_venda,
+                f_valor_venda,
+                i_cliente_cliente
+            })
+
+            return res.status(201).json(newVenda);
         }catch(error){
-            return res.status(500).json({error: error.message})
+            console.error("Error on HTTP method to create venda:", error.message);
+            return res.status(500).json({error:error.message})
         }
     }
 }
--- src/routes/venda.route.js
+++ src/routes/venda.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/", (req,res) => vendaController.getAllVendas(req,res));
 router.get("/:id" , (req,res) => vendaController.getVendaById(req,res));
+router.post("/", (req,res) => vendaController.createVenda(req,res))
 
 module.exports = router;
 
