e5a7cb8305bbde5820449e223294b69340fb55eb
NovettiGustavo <gustavonovettidev@gmail.com>
2025-09-17 14:26:08 -0300

    Criada classes centralizadoras para tratamentos de erros e iniciando tratamente de cors

---
+++ src/utils/errors/index.js
@@ -0,0 +1,9 @@
+const AppError = require("./appError");
+const ValidationError = require("./validationError");
+const NotFoundError = require("./notFoundError");
+
+module.exports = {
+  AppError,
+  ValidationError,
+  NotFoundError,
+};
